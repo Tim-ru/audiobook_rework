f7dbf169ce577a5d0af05d29811be0602cceb0c8
timoha176 <httimoha@gmail.com>
2021-07-27 19:31:56 +0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {styles} from './src/styles'
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.container}>App</Text>
+    </View>
+  );
+};
+
+
+
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        color: "blue",
+        fontSize: 20
+    }
+})
